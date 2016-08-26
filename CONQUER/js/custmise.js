a1460aa39ac219405b0db4580cba8ea47489fe66
Abedelrahman <abdelrrahmanmohsen@yahoo.com>
2016-08-26 05:48:05 +0200

    custmize

---
+++ CONQUER/js/custmise.js
@@ -0,0 +1,28 @@
+/*global $,alart, console */
+$(function () {
+	"use strict";
+//    smoth scroll div
+    $('.nav-justified li').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#" + $(this).data('value')).offset().top - 125
+        }, 2000);
+    });
+    $('.nav-justified li a').click(function () {
+        $(this).parents('li').addClass('active').siblings().removeClass('active');
+    });
+
+   	//Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+		if ($(this).scrollTop() > 150) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+});
